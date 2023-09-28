bcdf1c30283b1f1a751d9d5800165bb0f9588520
functionditi <aduneti.9626@gmail.com>
2023-09-28 16:39:00 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -164,20 +164,20 @@ function clearLightBox(){
 
 
 
-function touchStarted () {
-  if (!fullscreen()) {
-    fullscreen(true);
-  }
-}
+// function touchStarted () {
+//   if (!fullscreen()) {
+//     fullscreen(true);
+//   }
+// }
 
-/* full screening will change the size of the canvas */
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
+// /* full screening will change the size of the canvas */
+// function windowResized() {
+//   resizeCanvas(windowWidth, windowHeight);
+// }
 
-/* prevents the mobile browser from processing some default
- * touch events, like swiping left for "back" or scrolling the page.
- */
-document.ontouchmove = function(event) {
-    event.preventDefault();
-};
+// /* prevents the mobile browser from processing some default
+//  * touch events, like swiping left for "back" or scrolling the page.
+//  */
+// document.ontouchmove = function(event) {
+//     event.preventDefault();
+// };
