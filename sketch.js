aa3032913f7b79d25326fc657002d3d89bbffa15
functionditi <aduneti.9626@gmail.com>
2023-09-28 15:55:25 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -54,6 +54,7 @@ let locX, locY;
 let main;
 function preload(){
   home=loadImage('assets/home.png');
+  elements[1]=loadImage('assets/lightboxes/'+1+'.png');
   
   // for (let i=1; i<46; i++){
   //   elements[i]=loadImage('assets/lightboxes/'+i+'.png');
@@ -62,7 +63,7 @@ function preload(){
 
 function setup() {
   main=createCanvas(1080, 780);
-  main.mouseClicked(drawLightBox);
+  main.mouseClicked(checkLocation);
   lightbox=createGraphics(1080, 780);
   background(255);
   image(home, 0, 0, 1080, 780);
@@ -72,27 +73,27 @@ function setup() {
  
 }
 
-// function mouseClicked(){
-//   //console.log("clicked")
-//   console.log("elementBox.push({x:", locX, ", y:", locY, "});" );
-//   for (let i=0; i<elementBox.length; i++){
-//     let x=elementBox[i].x;
-//     let y=elementBox[i].y;
-//     if (locX>x && locY>y && locX<x+82 && locY<y+82 && flag==0){
-//       flag=1;
+function checkLocation(){
+  //console.log("clicked")
+  console.log("elementBox.push({x:", locX, ", y:", locY, "});" );
+  for (let i=0; i<elementBox.length; i++){
+    let x=elementBox[i].x;
+    let y=elementBox[i].y;
+    if (locX>x && locY>y && locX<x+82 && locY<y+82 && flag==0){
+      flag=1;
       
-//       drawLightBox(i);
-//     } 
+      drawLightBox(i);
+    } 
     
-//   }
+  }
 
 
-//   if (flag=1 && locX>80 && locY>100 && locX<=127 && locY<=143){
-//     flag=0;
-//     clearLightBox();
-//   }
+  if (flag=1 && locX>80 && locY>100 && locX<=127 && locY<=143){
+    flag=0;
+    clearLightBox();
+  }
  
-// }
+}
 
 function draw() {
   locX=mouseX;
@@ -103,7 +104,7 @@ function draw() {
 
 function drawLightBox(index){
   lightbox.background(0, 130);
-  //lightbox.image(elements[index+1], 78, 96.5, 921, 546);
+  lightbox.image(elements[index+1], 78, 96.5, 921, 546);
   image(lightbox, 0, 0);
 
 }
