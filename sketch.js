d966db0bbe2c764c2b68b7b09396976fcf7d4178
functionditi <aduneti.9626@gmail.com>
2023-09-28 14:49:11 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,6 +7,48 @@ let flag=0;
 elementBox.push({x: 46, y: 185});
 elementBox.push({x: 867 , y: 184 });
 elementBox.push({x: 949 , y: 184 });
+elementBox.push({x: 48 , y: 267 });
+elementBox.push({x: 130 , y: 268 });
+elementBox.push({x: 704 , y: 267 });
+elementBox.push({x: 786 , y: 267 });
+elementBox.push({x: 868 , y: 267 });
+elementBox.push({x: 951 , y: 267 });
+elementBox.push({x: 49 , y: 350 });
+ elementBox.push({x: 130 , y: 350 });
+ elementBox.push({x: 212 , y: 350 });
+ elementBox.push({x: 295 , y: 351 });
+ elementBox.push({x: 377 , y: 351 });
+ elementBox.push({x: 458 , y: 350 });
+ elementBox.push({x: 540 , y: 350 });
+ elementBox.push({x: 621 , y: 350 });
+ elementBox.push({x: 703 , y: 350 });
+ elementBox.push({x: 786 , y: 351 });
+ elementBox.push({x: 869 , y: 351 });
+ elementBox.push({x: 950 , y: 352 });
+ elementBox.push({x: 49 , y: 433 });
+ elementBox.push({x: 130 , y: 433 });
+ elementBox.push({x: 212 , y: 433 });
+ elementBox.push({x: 294 , y: 432 });
+ elementBox.push({x: 376 , y: 432 });
+ elementBox.push({x: 458 , y: 433 });
+ elementBox.push({x: 540 , y: 432 });
+ elementBox.push({x: 622 , y: 433 });
+ elementBox.push({x: 705 , y: 433 });
+ elementBox.push({x: 787 , y: 434 });
+ elementBox.push({x: 869 , y: 434 });
+ elementBox.push({x: 951 , y: 433 });
+ elementBox.push({x: 49 , y: 515 });
+ elementBox.push({x: 130 , y: 515 });
+ elementBox.push({x: 212 , y: 515 });
+ elementBox.push({x: 294 , y: 516 });
+ elementBox.push({x: 376 , y: 516 });
+ elementBox.push({x: 459 , y: 514 });
+ elementBox.push({x: 540 , y: 515 });
+ elementBox.push({x: 623 , y: 516 });
+ elementBox.push({x: 706 , y: 515 });
+ elementBox.push({x: 787 , y: 516 });
+ elementBox.push({x: 869 , y: 514 });
+ elementBox.push({x: 951 , y: 516 });
 
 let locX, locY;
 
@@ -29,12 +71,12 @@ function setup() {
 }
 
 function mouseClicked(){
-  console.log("clicked")
+  //console.log("clicked")
   console.log("elementBox.push({x:", locX, ", y:", locY, "});" );
   for (let i=0; i<elementBox.length; i++){
     let x=elementBox[i].x;
     let y=elementBox[i].y;
-    if (locX>x && locY>y && locX<=x+82 && locY<=y+82 && flag==0){
+    if (locX>x && locY>y && locX<x+82 && locY<y+82 && flag==0){
       flag=1;
       
       drawLightBox(i);
